b6ff410b1935b2a8639217f097aa58b5ed51e3fd
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-12-28 03:15:05 -0300

    added cl_enginefunc_t

--- include/SDK/BaseTypes.h
+++ include/SDK/BaseTypes.h
@@ -14,3 +14,5 @@ struct color24 { byte r; byte g; byte b; };
 struct colorVec { unsigned int r; unsigned int g; unsigned int b; unsigned int a; };
 typedef unsigned int CRC32_t;
 typedef unsigned long long uint64;
+typedef int HSPRITE;
+typedef struct rect_s { int left; int right; int top; int bottom; } wrect_t;
