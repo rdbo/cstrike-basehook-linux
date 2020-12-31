7749b0b555d6f2f1d821297f56b71ff1e11decef
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-12-31 14:43:08 -0300

    Add files via upload

--- include/SDK/BaseTypes.h
+++ include/SDK/BaseTypes.h
@@ -1,12 +1,17 @@
 #include <SDL2/SDL.h>
 
+#ifdef _WIN
+#include <Windows.h>
+#else
 typedef void* HWND;
 typedef int   BOOL;
+typedef unsigned long ULONG;
+#endif
+
 typedef float vec3_t[3];
 typedef unsigned int string_t;
 typedef float vec_t;
 typedef int int32;
 typedef int qboolean;
-typedef unsigned long ULONG;
 typedef unsigned char byte;
 typedef unsigned long long uint64;
